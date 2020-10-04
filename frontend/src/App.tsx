74c4090ac7fe1c49d1f51ab0304c89f24619fd42
Lukas Tveiten <lukasnt@stud.ntnu.no>
2020-10-04 14:13:45 +0200

    #11 - Added UI component for login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Login from './components/login';
 import Registration from './components/registration';
 
 function App() {
   return (
     <div>
+      <Login />
       <Registration />
     </div>
   );
--- frontend/src/components/registration/index.tsx
+++ frontend/src/components/registration/index.tsx
@@ -1,17 +1,18 @@
 import { Button, Paper, TextField } from '@material-ui/core';
 import React from 'react';
+import { postData } from '../utils/ajax';
 import './index.css';
 
 const Registration : React.FunctionComponent = () => {
   
   function register() : void {
-    const email : string = (document.getElementById("email") as HTMLInputElement).value;
-    const firstName : string = (document.getElementById("firstName") as HTMLInputElement).value;
-    const lastName : string = (document.getElementById("lastName") as HTMLInputElement).value;
-    const password : string = (document.getElementById("password") as HTMLInputElement).value;
-    const confirmPassword : string = (document.getElementById("confirmPassword") as HTMLInputElement).value;
+    const email : string = (document.getElementById("rEmail") as HTMLInputElement).value;
+    const firstName : string = (document.getElementById("rFirstName") as HTMLInputElement).value;
+    const lastName : string = (document.getElementById("rLastName") as HTMLInputElement).value;
+    const password : string = (document.getElementById("rPassword") as HTMLInputElement).value;
+    const confirmPassword : string = (document.getElementById("rConfirmPassword") as HTMLInputElement).value;
 
-    const data = {
+    const data : any = {
       email: email,
       firstName: firstName,
       lastName: lastName,
@@ -20,25 +21,17 @@ const Registration : React.FunctionComponent = () => {
     };
     console.log(JSON.stringify(data));
 
-    fetch("http://localhost:8080/api/user/register", {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-        //'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: JSON.stringify(data),
-    }).then(response => {
-      console.log(response);
-    });
+    postData("http://localhost:8080/api/user/register", data)
+      .then(res => console.log(res));
   }
 
   return (
       <Paper className="registration">
-        <TextField required id="email" label="Email" variant="outlined"/>
-        <TextField required id="firstName" label="First Name" variant="outlined"/>
-        <TextField required id="lastName" label="Last Name" variant="outlined"/>
-        <TextField required id="password" label="Password" variant="outlined" type="password"/>
-        <TextField required id="confirmPassword" label="Confirm Password" variant="outlined" type="password"/>
+        <TextField required id="rEmail" label="Email" variant="outlined"/>
+        <TextField required id="rFirstName" label="First Name" variant="outlined"/>
+        <TextField required id="rLastName" label="Last Name" variant="outlined"/>
+        <TextField required id="rPassword" label="Password" variant="outlined" type="password"/>
+        <TextField required id="rConfirmPassword" label="Confirm Password" variant="outlined" type="password"/>
         <Button variant="contained" color="primary" onClick={register}> Register </Button>
       </Paper>
   );
